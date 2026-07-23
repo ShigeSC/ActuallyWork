b84522cfb104c5112dd18a3c75f2e85b1921843b
ShigeSC <shigetora1205@gmail.com>
2026-07-23 23:23:42 +0800

    Update MAGANDAU.lua

--- MAGANDAU.lua
+++ MAGANDAU.lua
@@ -291,7 +291,7 @@ local function OpenClose()
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0.1021, 0, 0.0743, 0),
 		Size = UDim2.new(0, 50, 0, 50),
-		Image = "rbxassetid://8620551404",
+		Image = "rbxassetid://90541504618217",
 		Visible = true,
 	}, ScreenGui)
 
